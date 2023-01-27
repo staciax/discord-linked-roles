c1c68bf7617c2ac102e6303214a7a27d3850ac7c
STACIA <69411527+staciax@users.noreply.github.com>
2023-01-27 15:25:59 +0700

    update fastapi example

--- examples/fastapi_.py
+++ examples/fastapi_.py
@@ -1,32 +1,51 @@
 import datetime
 import logging
+from typing import Optional
 
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse, RedirectResponse
+from pydantic import BaseModel, Field
 
 import _config
-from linked_roles import (
-    LinkedRolesOAuth2,
-    OAuth2Scopes,
-    OAuth2Unauthorized,
-    RateLimited,
-    RolePlatform,
-    Unauthorized,
-    UserNotFound,
-)
+from linked_roles import LinkedRolesOAuth2, OAuth2Scopes, OAuth2Unauthorized, RolePlatform, User, UserNotFound
 
 _log = logging.getLogger(__name__)
 
 app = FastAPI(title='Linked Roles API', version='1.1.0')
 
-client = LinkedRolesOAuth2(
-    client_id=_config.DISCORD_CLIENT_ID,
-    client_secret=_config.DISCORD_CLIENT_SECRET,
-    redirect_uri=_config.DISCORD_REDIRECT_URI,
-    token=_config.DISCORD_TOKEN,
-    scopes=(OAuth2Scopes.role_connection_write, OAuth2Scopes.identify),
-    state=_config.COOKIE_SECRET,
-)
+
+class LinkedRolesClient(LinkedRolesOAuth2):
+    def __init__(self):
+        super().__init__(
+            client_id=_config.DISCORD_CLIENT_ID,
+            client_secret=_config.DISCORD_CLIENT_SECRET,
+            redirect_uri=_config.DISCORD_REDIRECT_URI,
+            token=_config.DISCORD_TOKEN,
+            scopes=(OAuth2Scopes.role_connection_write, OAuth2Scopes.identify),
+            state=_config.COOKIE_SECRET,
+        )
+
+    async def on_user_application_role_connection_update(
+        self, user: User, before: RolePlatform, after: RolePlatform
+    ) -> None:
+        _log.info(f'User {user} updated their role connection from {before} to {after}')
+
+
+client = LinkedRolesClient()
+
+
+class Player(BaseModel):
+    name: str = Field(..., max_length=19)
+    tag: str = Field(..., max_length=5)
+    matches: int = Field(..., ge=0)
+    winrate: float = Field(..., ge=0, le=100)
+    combat_score: int = Field(..., ge=0)
+    competitive_rank: Optional[str] = Field(None)
+    owner_id: int = Field(..., ge=0)
+
+    @property
+    def display_name(self) -> str:
+        return self.name + '#' + self.tag
 
 
 @app.on_event('startup')
@@ -50,67 +69,81 @@ async def linked_roles():
 @app.get('/verified-role')
 async def verified_role(code: str):
 
+    # get tokens
     tokens = await client.get_oauth2_tokens(code)
+
+    # get user
     user = await client.fetch_user(tokens=tokens)
 
     if user is None:
         raise UserNotFound('User not found')
 
-    platform = RolePlatform(name='VALORANT', username='STACIA#1234')
-    platform.add_metadata(key='matches', value=100)
-    platform.add_metadata(key='winrate', value=50)
-    platform.add_metadata(key='combat_score', value=10)
-    platform.add_metadata(key='last_update', value=datetime.datetime.now())
-    platform.add_metadata(key='verified', value=True)
+    # set role platform
+    platform = RolePlatform(name='VALORANT', username=str(user))
 
+    # add metadata
+    platform.add_metadata(key='matches', value=10)
+    platform.add_metadata(key='winrate', value=20)
+    platform.add_metadata(key='combat_score', value=30)
+
+    # set role metadata
     await user.edit_role_metadata(platform=platform)
 
-    return 'Verified role successfully'
+    return '<h1>Role metadata set successfully. Please check your Discord profile.</h1>'
 
 
 @app.post('/update-role-metadata')
-async def update_role_metadata(user_id: str):
+async def update_role_metadata(player: Player):
 
-    user = client.get_user(id=user_id)
+    # get user to make sure they are still connected
+    user = client.get_user(id=player.owner_id)
 
     if user is None:
-        raise UserNotFound(f'User with ID {user_id} not found')
+        raise UserNotFound(f'User with ID {player.owner_id} not found')
 
+    # get tokens to make sure they are still authenticated
     tokens = user.get_tokens()
     if tokens is None:
-        raise OAuth2Unauthorized('User is not connected to Linked Roles')
+        raise OAuth2Unauthorized(f'User ID {player.owner_id} is not authenticated')
 
-    if tokens.is_expired():
-        await tokens.refresh()
+    # get user role platform
+    before = user.get_role_platform()
 
-    platform = user.get_role_platform()
-    if platform is None:
-        platform = RolePlatform(name='VALORANT', username=user.username)
-    platform.edit_metadata(key='matches', value=5000)
-    platform.edit_metadata(key='winrate', value=5000)
-    platform.edit_metadata(key='combat_score', value=100000)
-    platform.edit_metadata(key='last_update', value=datetime.datetime.now())
-    platform.edit_metadata(key='verified', value=True)
+    if before is None:
+        raise UserNotFound(f'User with ID {player.owner_id} has no role connection')
 
+    # copy role platform because we don't want to edit the original
+    platform = before.copy()
+
+    platform.username = player.display_name
+    if player.competitive_rank is not None:
+        platform.username += f' ({player.competitive_rank})'
+
+    # edit metadata
+    platform.edit_metadata(key='matches', value=player.matches)
+    platform.edit_metadata(key='winrate', value=int(player.winrate))
+    platform.edit_metadata(key='combat_score', value=int(player.combat_score))
+
+    # new metadata
+    platform.add_or_edit_metadata(key='last_update', value=datetime.datetime.now())
+    platform.add_or_edit_metadata(key='verified', value=True)
+
+    # update role metadata
     await user.edit_role_metadata(platform=platform)
 
-
-@app.exception_handler(UserNotFound)
-async def unauthorized_exception_handler(r: Request, e: Unauthorized):
     return JSONResponse(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        content={'error': 'Unauthorized', 'message': e.message},
+        status_code=status.HTTP_200_OK,
+        content={
+            'status': 200,
+            'message': 'Updated role metadata successfully',
+            'user': player.owner_id,
+            'platform': {'before': before.to_dict(), 'after': platform.to_dict()},
+        },
     )
 
 
-@app.exception_handler(UserNotFound)
-async def not_found_exception_handler(r: Request, e: UserNotFound):
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={'error': 'Not found', 'message': e.message})
-
-
-@app.exception_handler(RateLimited)
-async def rate_limited_exception_handler(r: Request, e: RateLimited):
-    return JSONResponse(
-        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
-        content={'error': 'Rate limited', 'retry_after': e.retry_after, 'message': e.message},
-    )
+@app.post('/authenticated')
+async def is_is_authenticated(discord_id: int):
+    # check if user is authenticated
+    user = client.get_user(discord_id)
+    return JSONResponse(status_code=status.HTTP_200_OK, content={'status': 200, 'authenticated': user is not None})
