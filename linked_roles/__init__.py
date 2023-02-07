0a15282a54911d7aeed4b12c2fd9cd4c7b15f96f
STACIA <69411527+staciax@users.noreply.github.com>
2023-02-07 09:46:25 +0700

    bump 1.3.2

--- linked_roles/__init__.py
+++ linked_roles/__init__.py
@@ -10,7 +10,7 @@ __title__ = 'linked_roles'
 __author__ = 'staciax'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2023-present staciax'
-__version__ = '1.3.1'
+__version__ = '1.3.2'
 
 from . import utils as utils
 from .client import *
